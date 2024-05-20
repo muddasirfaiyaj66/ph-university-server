1dd24394fa6b24f6d18abe17771ef79dc73a691c
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-05-20 15:28:31 +0600

    [done] create student feature

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ async function main() {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Server is listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
