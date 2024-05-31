d9cc9a74b2566abfd784c9c253e80cf436e4009f
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-05-31 22:20:05 +0600

    [fix](studentId):fixed semester student year and semester code related problem

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -10,16 +10,29 @@ const findLastStudentId = async () => {
       _id: 0,
     },
   )
-  .sort({
-    createdAt:-1,
-  })
-  
-  .lean();
+    .sort({
+      createdAt: -1,
+    })
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    .lean();
+
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId =await findLastStudentId() || (0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId();
+  // 2030 01 0001
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
