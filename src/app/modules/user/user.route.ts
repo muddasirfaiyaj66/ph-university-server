c0f73298831c604f9d741fae8b3a829e8595aa9c
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-06-18 12:20:17 +0600

    [fet]: faculty and admin api created

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,6 +3,8 @@ import { UserController } from './user.controller';
 
 import { studentValidations } from '../student/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
+import { createAdminValidationSchema } from '../admin/admin.validation';
+import { createFacultyValidationSchema } from '../faculty/faculty.validation';
 
 const router = express.Router();
 
@@ -11,6 +13,17 @@ router.post(
   '/create-student',
   validateRequest(studentValidations.createStudentValidationSchema),
   UserController.createStudent,
+);
+router.post(
+  '/create-faculty',
+  validateRequest(createFacultyValidationSchema),
+  UserController.createFaculty,
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserController.createAdmin,
 );
 
 export const UserRoutes = router;
